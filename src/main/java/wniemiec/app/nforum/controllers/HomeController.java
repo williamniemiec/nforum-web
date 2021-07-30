9120d7c15f0efb1cab7a67dab4cf91c27ebd2809
William Niemiec <williamniemiec@hotmail.com>
2021-07-29 21:02:19 -0300

    Update HomeController.java

--- src/main/java/wniemiec/app/nforum/controllers/HomeController.java
+++ src/main/java/wniemiec/app/nforum/controllers/HomeController.java
@@ -2,11 +2,11 @@ package wniemiec.app.nforum.controllers;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import wniemiec.app.nforum.services.AuthService;
 
 @WebServlet("/home")
