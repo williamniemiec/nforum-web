6ccd60f4f1087094306d8c28ce2624a5b129afe4
William Niemiec <williamniemiec@hotmail.com>
2021-07-29 21:12:43 -0300

    Update RankingController.java

--- src/main/java/wniemiec/app/nforum/controllers/RankingController.java
+++ src/main/java/wniemiec/app/nforum/controllers/RankingController.java
@@ -3,11 +3,11 @@ package wniemiec.app.nforum.controllers;
 import java.io.IOException;
 import java.util.List;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import wniemiec.app.nforum.core.ViewLoader;
 import wniemiec.app.nforum.dto.UserDTO;
 import wniemiec.app.nforum.services.AuthService;
