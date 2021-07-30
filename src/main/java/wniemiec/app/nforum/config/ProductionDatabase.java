7057df04f5a5c6d5ada604a8215437b51d21f24e
William Niemiec <williamniemiec@hotmail.com>
2021-07-29 21:36:53 -0300

    Update ProductionDatabase.java

--- src/main/java/wniemiec/app/nforum/config/ProductionDatabase.java
+++ src/main/java/wniemiec/app/nforum/config/ProductionDatabase.java
@@ -9,7 +9,7 @@ public class ProductionDatabase implements Database {
 	
 	@Override
 	public String getUri() {
-		return "postgresql://jrctucgfmasimh:6e44e97bcbe891fc33e0e673f0d65ab143522a33c7683e221b9a82dddc6b8403@ec2-3-212-75-25.compute-1.amazonaws.com:5432/dbjglsh5vl0jup?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory&sslmode=require";
+		return "jdbc:postgresql://ec2-3-212-75-25.compute-1.amazonaws.com:5432/dbjglsh5vl0jup?password=6e44e97bcbe891fc33e0e673f0d65ab143522a33c7683e221b9a82dddc6b8403&sslmode=require&user=jrctucgfmasimh";
 	}
 	
 	@Override
